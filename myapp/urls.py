b3fd9e70f1801f0f146090c99bfaa988e1479e58
Adong520 <a18758@hotmail.com>
2018-04-19 15:48:00 +1000

    add Post and Note

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('post', views.post_list, name='post_list'),
+    path('note', views.note_list, name='note_list'),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post, Note
 
-# Create your views here.
+
 def home(request):
     return render(request, 'myapp/home.html',{})
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'myapp/post_list.html',{'posts':posts})
+
+
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, 'myapp/note_list.html',{'notes':notes})
