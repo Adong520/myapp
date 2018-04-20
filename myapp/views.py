0948fc549340926391264bedeee88be96c290700
Adong520 <a18758@hotmail.com>
2018-04-20 16:56:26 +1000

    add form for new and edit function

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Note
+from .forms import PostForm
+
 
 
 def home(request):
@@ -12,6 +14,44 @@ def post_list(request):
     return render(request, 'myapp/post_list.html',{'posts':posts})
 
 
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'myapp/post_detail.html',{'post':post} )
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.creation_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request,'myapp/post_edit.html', {'form':form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.creation_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+        return render(request,'myapp/post_edit.html',{'form':form})
+
+
+
+
 def note_list(request):
     notes = Note.objects.all()
     return render(request, 'myapp/note_list.html',{'notes':notes})
+
